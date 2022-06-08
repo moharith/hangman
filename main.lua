ac265fa91a517f20a5328e7e687e3aa412234d17
Jordi <geusjm@gmail.com>
2022-06-08 21:15:30 +0200

    v1

--- main.lua
+++ main.lua
@@ -1,89 +1,130 @@
-woord = "waterpomp"
-woordarray = { }
-geradenletters = { } 
+LETTER_A = 97
+LETTER_Z = 122
 
-local letterkeuzes = string
-local letters = string
-score = 5
+WINDOW_HEIGHT = 768
+WINDOW_WIDTH = 1024
+
+local fontGroot = love.graphics.newFont(30)
+local ingedrukteToets = key
+
+
+local woord = "boomkever"
+local woordlengte = string.len(woord)
+
+local tabel = {}
+local alfabet = {}
+
+
 
 function love.load()
-love.window.setMode(1024, 768, {
-    vsync = true,
-    fullscreen = false,
-    resizable = true
-})
+    --
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT)
+    love.window.setTitle('Galgje')
 
-    woordlengte = string.len(woord)
-
-    for char in string.gmatch(woord, ".") do
-        woordarray[#woordarray+1] = char
+    for i = LETTER_A, LETTER_Z do
+        table.insert(alfabet, {letter = i, status = "neutraal"})
     end
-
 end
 
-function woordcheck()
-    local match 
-    for i=1, #woordarray do
-        if letterkeuze == geradenletters[i]
-            then 
-                match = false
-        end
-    end
-
-    if match == false then
-                score = score - 1
-    else
-return
-    end
-            match = 0
-end
-
-function love.keypressed(key)
-    if key and key:match( '%a' ) then character = key 
-        local i = 0
-        letterkeuze = key
-        woordcheck()
-        table.insert(geradenletters,letterkeuze)
-    end
-
-
-    
-
-    if key == " return " then
+function love.keypressed(key, scancode)
+    ingedrukteToets = tostring(key)
+    if key == 'escape' then
         love.event.quit()
     end
-end
-
-function love.update(dt)
-    if score == 0 then
-        --  love.event.quit()
-    end 
 
 
 
-end
 
-function love.draw()
-love.graphics.print(score, 100, 100 , 100)
-    --- print strepen voor letterkeuzes
-    for k, v in pairs(woordarray) do
-        love.graphics.printf("_", 10 * k, 10, 200)
-   --     love.graphics.printf(woordarray[k], 10, 20 * k, 200)
-
-    end
-
-
-    -- per letter in woord
-        for k, letters in pairs(woordarray) do
-            -- per geraden letter
-            for o, inhoud in pairs(geradenletters) do
-                if letters == geradenletters[o] then
-                    love.graphics.printf(letters, 10 * k, 10, 200)
+    for i = 1, woordlengte do 
+        if string.byte(ingedrukteToets) ~= tabel[i].lettercode then
+            for k, v in pairs(alfabet) do
+                if alfabet[k].letter == string.byte(ingedrukteToets) then
+                alfabet[k].status = "fout"
                 end
             end
         end
-     --   x = false
-   -- end
+    end
+
+    for i = 1, woordlengte do 
+        if string.byte(ingedrukteToets) == tabel[i].lettercode then
+            tabel[i].show = "ja"
+            for k, v in pairs(alfabet) do
+                
+                if alfabet[k].letter == tabel[i].lettercode  then
+                  --  love.graphics.setColor(0,1,0,1)
+                  alfabet[k].status = "goed"
+                end
+
+            end
+        end
+    end
+
+
 
 end
 
+function wow()
+    local x = 0
+    local n = 0
+   
+    
+
+    for i = LETTER_A, LETTER_Z do
+        n = n + 1
+
+        if alfabet[n].status == "neutraal" then
+            love.graphics.setColor(0,0,0,0)
+        end
+        if alfabet[n].status == "goed" then
+            love.graphics.setColor(0,1,0,1)
+        end
+        if alfabet[n].status == "fout" then
+            love.graphics.setColor(1,0,0,1)
+        end        
+        love.graphics.printf(string.char(i), x - 40, WINDOW_HEIGHT - 150, 200, "center")
+        x = x + 35       
+
+    end
+    love.graphics.setColor(1,1,1,1)
+
+end
+
+
+
+function love.draw()
+    love.graphics.setFont(fontGroot)
+  
+
+    love.graphics.print(string.byte(tostring(ingedrukteToets)))
+    love.graphics.print(alfabet[2].letter, 70, 70)
+
+    for i = 1, woordlengte do
+        i = string.sub(woord, i, i) 
+        ichar = string.byte(i)
+        table.insert(tabel, { lettercode = ichar, show="nee"})
+    end
+
+
+
+
+    woordstring()
+
+    -- for i = 1, woordlengte do
+    --     if string.byte(ingedrukteToets) == tabel
+    -- end
+
+end
+
+function woordstring()
+    for i = 1, woordlengte do   
+        if tabel[i].show == "ja" then
+            love.graphics.setColor(1,1,1,1)
+            love.graphics.print((string.char(tabel[i].lettercode)), 200 + (35 * i), 400)
+        else 
+            love.graphics.setColor(0,0,0,0)
+        end
+    end
+
+    wow()
+   love.graphics.setColor(1,1,1,1)
+end
